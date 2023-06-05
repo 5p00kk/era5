4610082e2cdacfcb392ab9dc814aa7d2809bb878
5p00kk <szymon.kowalewski@gmail.com>
2023-06-05 17:52:41 +0200

    update

--- era.py
+++ era.py
@@ -4,7 +4,7 @@ import pygrib
 import os
 
 FILE_NAME = "download.grib"
-DOWNLOAD = os.path.exists("download.grib")
+DOWNLOAD = not os.path.exists("download.grib")
 REQUEST = {
     "variable": "2m_temperature",
     "product_type": "monthly_averaged_reanalysis",
@@ -21,24 +21,21 @@ if DOWNLOAD:
 grbs = pygrib.open('download.grib')
 
 # Available GRBS
+print("Available msgs:")
 for grb in grbs:
     print(grb)
-#for key in grbs[1].keys():
-#    print(key)
+
 
 grbs.rewind() # rewind the iterator
-i = 0
-for grb in grbs:
+for i, grb in enumerate(grbs):
+    print(f"\nMsg {i} ({grb})")
+
     lats, lons = grb.latlons()
     
-    print(grb.values.shape)
-    print(lats.shape)
-    print(lons.shape)
-
-    print(f"{grb.values.min()} {grb.values.max()}")
-    print(f"{lats.min()} {lats.max()}")
-    print(f"{lons.min()} {lons.max()}")
-    
+    print(f"Values: {grb.values.shape} [{grb.values.min()}-{grb.values.max()}]")
+    print(f"Lats: {lats.shape} [{lats.min()}-{lats.max()}]")
+    print(f"Lons: {lons.shape} [{lons.min()}-{lons.max()}]")
+   
     #for j in range(len(grb.values)):
         #print(f"{i}, {j}: {len(lats[j])}, {len(lons[j])}, {len(grb.values[j])}")
 

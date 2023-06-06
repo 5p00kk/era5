ac158b27e9b86345273ac3cfb012267425f46b20
5p00kk <szymon.kowalewski@gmail.com>
2023-06-06 11:24:21 +0200

    add header to csv file

--- utils.py
+++ utils.py
@@ -3,8 +3,9 @@ import csv
 def load_locations(file_name):
     locations = {}
     with open(file_name, newline='') as csvfile:
-        cav_reader = csv.reader(csvfile, delimiter=',')
-        for row in cav_reader:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        next(csv_reader, None)
+        for row in csv_reader:
             name = row[0]
             lat = float(row[1])
             lon = float(row[2])

97ffd88a14336dd71e186a5d8a84cb56a90bb4c9
5p00kk <szymon.kowalewski@gmail.com>
2023-06-06 11:33:36 +0200

    add progress bar

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from era_data import Era_data
 from visualizer import Visualizer
 from logger import Logger, Logger_level
 from utils import load_locations
+from tqdm import tqdm
 
 # Init objects
 data_loader = Era_data("2m_temperature")
@@ -26,33 +27,39 @@ logger.info(f"Lat: {location['lat']} Lon: {location['lon']}")
 
 total_data = []
 year_data = []
+total_it = len(data_loader.year_list)*12
 
 grb_data.rewind() # rewind the iterator
-for i, grb in enumerate(grb_data):
+with tqdm(total=total_it) as progress_bar:
+    for i, grb in enumerate(grb_data):
+        
+        # Update progress bar
+        progress_bar.set_description(f"{grb.year}")
+        progress_bar.update(1)
 
-    # Get lat/lon values
-    lats, lons = grb.latlons()
-    # Make sure they match
-    assert(grb.values.shape == lats.shape)
-    assert(lons.shape == lats.shape)
-    # Get single row/col as they repeat
-    lats = lats[:,0]
-    lons = lons[0]
+        # Get lat/lon values
+        lats, lons = grb.latlons()
+        # Make sure they match
+        assert(grb.values.shape == lats.shape)
+        assert(lons.shape == lats.shape)
+        # Get single row/col as they repeat
+        lats = lats[:,0]
+        lons = lons[0]
 
-    # Find closest lat/lon
-    lat_idx = np.argmin(abs(lats-location["lat"]))
-    lon_idx = np.argmin(abs(lons-location["lon"]))
+        # Find closest lat/lon
+        lat_idx = np.argmin(abs(lats-location["lat"]))
+        lon_idx = np.argmin(abs(lons-location["lon"]))
 
-    # Extract temp value
-    # TODO: interpolation
-    temp_k = grb.values[lat_idx][lon_idx]
-    temp_c = temp_k -272.15
+        # Extract temp value
+        # TODO: interpolation
+        temp_k = grb.values[lat_idx][lon_idx]
+        temp_c = temp_k -272.15
 
-    year_data.append(temp_c)
-    
-    if grb.month == 12:
-        logger.info(f"{grb.year}")
-        total_data.append(year_data)
-        year_data = []
+        year_data.append(temp_c)
+        
+        if grb.month == 12:
+            #logger.info(f"{grb.year}")
+            total_data.append(year_data)
+            year_data = []
 
-visualizer.visu(total_data, data_loader.year_list)
+    visualizer.visu(total_data, data_loader.year_list)

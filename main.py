986c29703cfc5d5a194410c86279f35626c30070
5p00kk <szymon.kowalewski@gmail.com>
2023-06-07 11:05:11 +0200

    many changes

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import numpy as np
 from era_data import Era_data
-from visualizer import Visualizer
 from logger import Logger, Logger_level
-from utils import load_locations
+from utils import load_locations, window_avg, deviation, plot_heatmap
 from tqdm import tqdm
 
 # Init objects
 data_loader = Era_data("2m_temperature")
 logger = Logger(Logger_level.INFO)
-visualizer = Visualizer(5)
 
 # Load ERA data
 grb_data = data_loader.load()
@@ -27,7 +25,7 @@ for location in locations.values():
     total_data = []
     year_data = []
 
-    logger.info(f"Extracting data for: {location['name'].upper()}")
+    logger.info(f"\nExtracting data for: {location['name'].upper()}")
     logger.info(f"Lat: {location['lat']} Lon: {location['lon']}")
 
     # TODO this loop can be done only once
@@ -56,7 +54,7 @@ for location in locations.values():
             # Extract temp value
             # TODO: interpolation
             temp_k = grb.values[lat_idx][lon_idx]
-            temp_c = temp_k -272.15
+            temp_c = temp_k-272.15
 
             year_data.append(temp_c)
             
@@ -65,5 +63,16 @@ for location in locations.values():
                 total_data.append(year_data)
                 year_data = []
 
-    logger.info(f"Temp range: {min(min(total_data))} to {max(max(total_data))}")
-    visualizer.visu(total_data, data_loader.year_list, location["name"])
+    # Calculate averaged total and deviation data
+    WS = 3
+    total_data = np.array(total_data, dtype=np.float64)
+    dev_data = deviation(total_data)
+    total_data_avg = window_avg(total_data, WS)
+    dev_data_avg = window_avg(dev_data, WS)
+    minmax = max(abs(dev_data_avg.min()), abs(dev_data_avg.min()))
+
+    logger.info(f"Temp range: {total_data.min()} to {total_data.max()}")
+    logger.info(f"Temp deviation range: {dev_data_avg.min()} to {dev_data_avg.max()}")
+
+    plot_heatmap(total_data_avg, data_loader.year_list[WS:], location["name"], "t", -40, 40)
+    plot_heatmap(dev_data_avg, data_loader.year_list[WS:], location["name"], "d", -1*minmax, minmax)
--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import csv
+import numpy as np
+import seaborn
+import matplotlib.pyplot as plt
+import os
 
 def load_locations(file_name):
     locations = {}
@@ -11,3 +15,34 @@ def load_locations(file_name):
             lon = float(row[2])
             locations[name] = {"name": name, "lat": lat, "lon": lon}
     return locations
+
+def deviation(data):
+    dev_data = np.zeros(data.shape, dtype=np.float)
+    for col in range(data.shape[1]):
+        col_avg = np.average(data[:,col])
+        dev_data[:,col] = data[:,col]-col_avg
+    return dev_data
+
+def window_avg(data, ws):
+    output_shape = ((data.shape[0] - ws + 1, data.shape[1]))
+    output = np.zeros(output_shape)
+    for j in range(output_shape[1]):
+        for i in range(output_shape[0]):
+            window = data[i:(i+ws), j]
+            output[i,j] = np.mean(window)
+    return output
+
+def plot_heatmap(data, y_labels, title, prefix="", min=None, max=None):
+    ax = seaborn.heatmap(data, vmin=min, vmax=max, cmap='coolwarm')
+    # Set x and y ticks
+    ax.set_yticks(range(0, len(y_labels), 10))
+    ax.set_yticklabels(y_labels[::10])
+    ax.set_xticklabels([1,2,3,4,5,6,7,8,9,10,11,12])
+    # Set titles
+    plt.title(title.upper())
+    plt.xlabel("month")
+    plt.ylabel("year")
+    # Save
+    filename = prefix+"_"+title+".png"
+    plt.savefig(os.path.join("output", filename))
+    plt.close()
